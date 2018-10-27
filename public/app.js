3a77ca18e391722f8e6aedd0b02a56ee44b30d85
dzimiks <vana997@gmail.com>
2018-10-27 21:24:16 +0200

    Add style and datatable

---
+++ public/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    // Datatable
+    $('#datatable').DataTable({
+        dom: "Blfrtip",
+        buttons: [
+            {
+                extend: "copy",
+                className: "btn-sm"
+            },
+            {
+                extend: "csv",
+                className: "btn-sm"
+            },
+            {
+                extend: "excel",
+                className: "btn-sm"
+            },
+            {
+                extend: "pdfHtml5",
+                className: "btn-sm"
+            }
+        ],
+        responsive: true
+    });
+});
