b118fb2d5093ecff69684593f6acc262d4eec830
dzimiks <vana997@gmail.com>
2019-08-20 17:54:02 +0200

    Finish

--- public/app.js
+++ public/app.js
@@ -22,4 +22,32 @@ $(document).ready(function () {
         ],
         responsive: true
     });
+
+    $('#schedule').DataTable({
+        dom: "Blfrtip",
+        buttons: [
+            {
+                extend: "copy",
+                className: "btn-sm"
+            },
+            {
+                extend: "csv",
+                className: "btn-sm"
+            },
+            {
+                extend: "excel",
+                className: "btn-sm"
+            },
+            {
+                extend: "pdfHtml5",
+                className: "btn-sm"
+            }
+        ],
+        responsive: true
+    });
+
+    // Sidebar
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
 });
